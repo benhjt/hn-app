94b3121448952ac537909bb1f06f911416b2983c
benhjt <benhjt@users.noreply.github.com>
2022-12-10 12:53:14 +0000

    Adds tailwind classes for responsive layout

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,18 +6,20 @@ import Feed from '../Feed';
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <main>
-        <Routes>
-          <Route path="/news/:page" element={<Feed feedType="news" />} />
-          <Route path="/newest/:page" element={<Feed feedType="newest" />} />
-          <Route path="/show/:page" element={<Feed feedType="show" />} />
-          <Route path="/ask/:page" element={<Feed feedType="ask" />} />
-          <Route path="/jobs/:page" element={<Feed feedType="jobs" />} />
-        </Routes>
-      </main>
-    </Router>
+    <div className="container mx-auto">
+      <Router>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/news/:page" element={<Feed feedType="news" />} />
+            <Route path="/newest/:page" element={<Feed feedType="newest" />} />
+            <Route path="/show/:page" element={<Feed feedType="show" />} />
+            <Route path="/ask/:page" element={<Feed feedType="ask" />} />
+            <Route path="/jobs/:page" element={<Feed feedType="jobs" />} />
+          </Routes>
+        </main>
+      </Router>
+    </div>
   );
 }
 
