b1652d8354026587de3549b7146d40d7020539f4
benhjt <benhjt@users.noreply.github.com>
2022-12-10 12:54:08 +0000

    Adds Basic styling to Header component including a Separator

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,17 +3,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import NavLink from '../NavLink';
 
-function Header() {
-  return (
-    <nav>
-      <Link to="/news/1">Logo Here</Link>
-      <NavLink to="/news/1">Top</NavLink>
-      <NavLink to="/newest/1">New</NavLink>
-      <NavLink to="/show/1">Show</NavLink>
-      <NavLink to="/ask/1">Ask</NavLink>
-      <NavLink to="/jobs/1">Jobs</NavLink>
-    </nav>
-  );
-}
+const Separator = () => <div className="h-5/6 w-px bg-black">&nbsp;</div>;
+
+const Header = () => (
+  <nav className="flex px-2 bg-orange-600">
+    <Link to="/news/1" className="font-semibold mr-3">
+      Hacker News
+    </Link>
+    <NavLink to="/news/1">Top</NavLink>
+    <Separator />
+    <NavLink to="/newest/1">New</NavLink>
+    <Separator />
+    <NavLink to="/show/1">Show</NavLink>
+    <Separator />
+    <NavLink to="/ask/1">Ask</NavLink>
+    <Separator />
+    <NavLink to="/jobs/1">Jobs</NavLink>
+  </nav>
+);
 
 export default Header;
