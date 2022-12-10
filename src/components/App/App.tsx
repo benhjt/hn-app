712986399293110930253547a2e4e84b9ba48603
benhjt <benhjt@users.noreply.github.com>
2022-12-10 11:36:35 +0000

    Wraps App component with react-router-dom

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from '../Header';
+import Feed from '../Feed';
+
 function App() {
   return (
-    <div>
-      hello world
-    </div>
+    <Router>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/news/:page" element={<Feed feedType="news" />} />
+          <Route path="/newest/:page" element={<Feed feedType="newest" />} />
+          <Route path="/show/:page" element={<Feed feedType="show" />} />
+          <Route path="/ask/:page" element={<Feed feedType="ask" />} />
+          <Route path="/jobs/:page" element={<Feed feedType="jobs" />} />
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
