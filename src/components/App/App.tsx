e948f6ea7be0fee3514b34e42878d022e191e248
benhjt <benhjt@users.noreply.github.com>
2022-12-11 01:49:51 +0000

    Adds redirect from root url to 'news' page

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import Header from '../Header';
 import Feed from '../Feed';
 
@@ -11,6 +16,7 @@ const App: React.FC = () => {
         <Header />
         <main>
           <Routes>
+            <Route path="/" element={<Navigate replace to="/news/1" />} />
             <Route path="/news/:page" element={<Feed feedType="news" />} />
             <Route path="/newest/:page" element={<Feed feedType="newest" />} />
             <Route path="/show/:page" element={<Feed feedType="show" />} />
