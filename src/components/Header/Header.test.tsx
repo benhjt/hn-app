33e0e6cf3b969db73be72d3a2a4a839a2dd1771f
benhjt <benhjt@users.noreply.github.com>
2022-12-11 01:39:45 +0000

    Adds describe wrapper

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -2,7 +2,9 @@ import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './Header';
 
-it('renders correctly', () => {
-  const { container } = render(<Header />, { wrapper: BrowserRouter });
-  expect(container).toMatchSnapshot();
+describe('Header', () => {
+  it('renders correctly', () => {
+    const { container } = render(<Header />, { wrapper: BrowserRouter });
+    expect(container).toMatchSnapshot();
+  });
 });
