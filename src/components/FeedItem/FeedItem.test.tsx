9d6364f20c284493ddec046e85484724f6423889
benhjt <benhjt@users.noreply.github.com>
2022-12-11 01:40:57 +0000

    Initial commit of FeedItem component

---
+++ src/components/FeedItem/FeedItem.test.tsx
@@ -0,0 +1,26 @@
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+
+import FeedItem from './FeedItem';
+
+const item = {
+  id: 33934580,
+  title: 'This cool story!',
+  points: 54,
+  user: 'username123',
+  time: 1670693712,
+  time_ago: '8 hours ago',
+  comments_count: 226,
+  type: 'link',
+  url: 'https://www.google.com',
+  domain: 'google.com',
+};
+
+describe('FeedItem', () => {
+  it('renders correctly', () => {
+    const { container } = render(<FeedItem item={item} index={1} />, {
+      wrapper: BrowserRouter,
+    });
+    expect(container).toMatchSnapshot();
+  });
+});
---
+++ src/components/FeedItem/FeedItem.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+type FeedItemData = {
+  id: number;
+  title: string;
+  points: number;
+  user: string;
+  time: number;
+  time_ago: string;
+  comments_count: number;
+  type: string;
+  url: string;
+  domain: string;
+};
+
+type FeedItemProps = {
+  item: FeedItemData;
+  index: number;
+};
+
+const FeedItem: React.FC<FeedItemProps> = ({ item, index }) => (
+  <div>
+    <div>{index}</div>
+    <div>
+      <div>
+        <a href={item.url} target="_blank" rel="noopener noreferrer">
+          {item.title}
+        </a>{' '}
+        (
+        <a href={item.domain} target="_blank" rel="noopener noreferrer">
+          {item.domain}
+        </a>
+        )
+      </div>
+      <div>
+        {item.points} points by{' '}
+        <Link to={`/user/${item.user}`}>{item.user}</Link> {item.time_ago} |{' '}
+        <Link to={`/story/${item.id}`}>{item.comments_count} comments</Link>
+      </div>
+    </div>
+  </div>
+);
+
+export default FeedItem;
